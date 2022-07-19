3c809e7c39b0dbf4b10a90449cab2070dc10cdd9
egeergull <ege.ergul@ug.bilkent.edu.tr>
2022-07-19 16:15:29 +0300

    end of exercise 1

--- TS_Exercise_05/app.ts
+++ TS_Exercise_05/app.ts
@@ -1,1 +1,17 @@
 console.log("Unknown type");
+
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Ege";
+
+// this if statement is necessary for unkown type
+if (typeof userInput === "string") userName = userInput;
+
+// you can directly assign type of any to a string
+let userInput2: any;
+userInput2 = 5;
+userInput2 = "Ege";
+
+userName = userInput2;
